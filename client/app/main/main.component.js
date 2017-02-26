45da8d56f25d7a00d59462ca6137946a13670b81
Rebecka Arieli <rebeckaarieli@Rebeckas-MacBook-Air.local>
2017-02-25 22:24:25 -0800

    seccond

--- client/app/main/main.component.js
+++ client/app/main/main.component.js
@@ -1,5 +1,5 @@
 // Code goes here
-
+'use strict';
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 import routing from './main.routes';
@@ -9,17 +9,15 @@ export class MainController {
   socket;
   awesomeThings = [];
   newThing = '';
-  SearchService;
 
   /*@ngInject*/
-  constructor($http, $scope, socket, SearchService) {
+  constructor($http, $scope, socket) {
     this.$http = $http;
     this.socket = socket;
     $scope.search = function() {
       SearchService.query(function(repositories) {
         $scope.repositories = repositories;
-        //socket.syncUpdates('repositories', $scope.repositories);
-        console.log(repositories)
+        socket.syncUpdates('repositories', $scope.repositories);
       })
 
     }
@@ -27,36 +25,14 @@ export class MainController {
       socket.unsyncUpdates('thing');
     });
   }
-
-
-  $onInit() {
-    this.$http.get('/api/things')
-      .then(response => {
-        this.awesomeThings = response.data;
-        this.socket.syncUpdates('thing', this.awesomeThings);
-      });
   }
 
-  addThing() {
-    if (this.newThing) {
-      this.$http.post('/api/things', {
-        name: this.newThing
-      });
-      this.newThing = '';
-    }
-  }
-
-  deleteThing(thing) {
-    this.$http.delete(`/api/things/${thing._id}`);
-  }
-}
 function SearchService($resource) {
   'ngInject';
-  return $resource('https://api/github.com/repositories/', {
+  return $resource('https://api/github.com/search/ever', {
       id: '@_id'
     })
   };
-
 
 export default angular.module('everstringApp.main', [uiRouter])
   .config(routing)
